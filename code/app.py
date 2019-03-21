282a4c6aaf315abc7932eaf97c01822501442ec3
Marianna <mckd2018@gmail.com>
2019-03-21 15:54:49 -0700

    add item & item list resources

--- code/app.py
+++ code/app.py
@@ -1,14 +1,37 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.security_key = 'jose'
 api = Api(app)
 
-class Student(Resource):
+items = []
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': 'item'}, 200 if item else 404
+
+    def post(self, name):
+        if next(filer(lambda x: x['name'] == name, items), None) is not None:
+            return {"message": "An item with name '{}' already exists.".format(name)}, 400
+
+        data = request.get_json(silent=True) # expecting that client gives you json
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
 
-api.add_resource(Student, '/student/<string:name>') #http://127.0.0.1:5000/student/Rolf
 
-app.run(port=5000)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
